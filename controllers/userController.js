d48da8aa57cd5d6343a2d24d86b62ad7d3c1081b
dev-TarunSingh <tarunsinghchauhan75@gmail.com>
2025-02-24 22:07:45 +0530

    Using bcrypt

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,10 @@
 import User from '../models/User.js';
 import dotenv from 'dotenv';
 dotenv.config();
-import simplecrypt from 'simplecrypt';
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
 
-const sc = simplecrypt();
+
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
@@ -16,7 +16,8 @@ export const signup = async (req, res) => {
     if (searchUser) {
       res.status(400).json({ message: 'User already exists' });
     } else {
-      const hashedPassword = await sc.encrypt(password);
+      const hashedPassword = await bcrypt.hash(password, 10);
+      console.log(hashedPassword);
       const newUser = await User.create({ email, password: hashedPassword, firstName });
       res.status(201).json({ message: 'User created successfully! Please Log In!' });
     }
@@ -33,14 +34,16 @@ export const login = async (req, res) => {
     if (!user) {
       return res.status(401).json({ message: 'Invalid credentials' });
     }
+    
     const isMatch = await bcrypt.compare(password, user.password);
     console.log(isMatch);
+    
     if (!isMatch) {
       return res.status(401).send('Invalid credentials');
     }
     const token = jwt.sign({ userId: user._id }, JWT_SECRET, { expiresIn: '1h' });
     console.log("LoggedIn");
-    res.status(200).json({ message: 'Logged in successfully', token });
+    res.status(200).json({ message: 'Logged in successfully', token, user : user });
   } catch (error) {
     res.status(500).send('Error logging in: ' + error.message);
   }
