130a09d5007fb6ae7066130b17fb1ca468a58063
dev-TarunSingh <tarunsinghchauhan75@gmail.com>
2025-07-27 16:34:34 +0530

    new notifcations

--- utils/sendPushNotification.js
+++ utils/sendPushNotification.js
@@ -1,5 +1,6 @@
 import { Expo } from "expo-server-sdk";
 
+// Create a new Expo SDK client
 const expo = new Expo();
 
 export const sendPushNotification = async (pushToken, message) => {
@@ -8,16 +9,17 @@ export const sendPushNotification = async (pushToken, message) => {
     return;
   }
 
+  const notification = {
+    to: pushToken,
+    sound: "default",
+    title: "New Message",
+    body: message.text || "You have a new message",
+    data: { chatroomId: message.chatroomId },
+  };
+
   try {
-    await expo.sendPushNotificationsAsync([
-      {
-        to: pushToken,
-        sound: "default",
-        title: "New Message",
-        body: message.text || "You have a new message",
-        data: { chatroomId: message.chatroomId },
-      },
-    ]);
+    const tickets = await expo.sendPushNotificationsAsync([notification]);
+    console.log("Push notification sent:", tickets);
   } catch (error) {
     console.error("Error sending push notification:", error);
   }
